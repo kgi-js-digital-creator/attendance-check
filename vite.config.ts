5dddadd3965d6cef7fe63252385930c883b4f31f
cadestoria <243375619+cadestoria@users.noreply.github.com>
2026-06-06 12:30:10 +0900

    baseの修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/attendance-check/',
   plugins: [react(), tailwindcss()],
   build: {
     license: true,
